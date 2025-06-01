9cddf4442bbba540ea6b77e41ba5a3ec8cff8b67
Alina Orlova <alinaorlova50@mail.ru>
2025-06-01 21:13:36 +0300

    Добавлено приложение для товаров

--- main/views.py
+++ main/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    return render(request, 'main/index.html')
+    context: dict[str, str] = {
+        'content': "Авторская печатная продукция"
+    }
+    return render(request, 'main/index.html', context)
