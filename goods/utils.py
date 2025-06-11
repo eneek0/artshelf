ee61a5536a3726e5c087ef23b60e916a1a1eff20
Alina Orlova <alinaorlova50@mail.ru>
2025-06-11 18:47:40 +0300

    Реализован полноценный поиск

--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,10 @@
 from django.db.models import Q
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 from goods.models import Products
 
@@ -6,11 +12,38 @@ def q_search(query):
     if query.isdigit():
         return Products.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("title", "description")
+    query = SearchQuery(query)
 
-    q_objects = Q()
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
-    for token in keywords:
-        q_objects |= Q(title__icontains=token)
+    result = result.annotate(
+        headline=SearchHeadline(
+            "title",
+            query,
+            start_sel='<span style="background-color: #F3CEFE;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: #F3CEFE;">',
+            stop_sel="</span>",
+        )
+    )
+    return result
+    
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    return Products.objects.filter(q_objects)
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(title__icontains=token)
+
+    # return Products.objects.filter(q_objects)
